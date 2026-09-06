d024fc9d0f2729944559604ab23bbac8ae00f048
Arctic403 <kc.campbell132@gmail.com>
2026-09-06 08:01:01 -0400

    Wire survival Local Play service worker modules

--- survival-local-play-sw.js
+++ survival-local-play-sw.js
@@ -1,1 +1,7 @@
-PLACEHOLDER
+/* Rift Survival Local Play service-worker entry. */
+'use strict';
+importScripts(
+  'survival-local-play-core.js?v=1',
+  'survival-local-play-authority.js?v=1',
+  'survival-local-play-routes.js?v=1'
+);
